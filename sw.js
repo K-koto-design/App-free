2392fb89ba5997061a9230e9f8f13128e750a43e
K-koto-design <tgkotokot@gmail.com>
2026-01-25 16:23:09 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
 const CACHE_NAME = 'mini-app-store-v1';
-
 const ASSETS = [
   './',
   './index.html',
@@ -7,32 +6,20 @@ const ASSETS = [
   './icon.png'
 ];
 
-// INSTALL
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 
-// ACTIVATE
-self.addEventListener('activate', event => {
-  event.waitUntil(
+self.addEventListener('activate', e => {
+  e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
-      )
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
     )
   );
   self.clients.claim();
 });
 
-// FETCH
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(cached =>
-      cached || fetch(event.request)
-    )
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
