0a8babef2ed7c43e77ecbaadfa4e2abc24fa54be
K-koto-design <tgkotokot@gmail.com>
2026-01-25 15:39:26 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,38 @@
-self.addEventListener("install", e => {
-  self.skipWaiting()
-})
+const CACHE_NAME = 'mini-app-store-v1';
 
-self.addEventListener("fetch", e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)))
-})
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
+// INSTALL
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// ACTIVATE
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// FETCH
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached =>
+      cached || fetch(event.request)
+    )
+  );
+});
